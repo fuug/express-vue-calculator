5889d44fda5ffabc5f791c6cbfd7967a7f592c50
skalaban <m.skalaban@gmail.com>
2023-05-24 23:00:03 +0300

    fix router

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,4 +1,6 @@
 import {createRouter, createWebHistory} from 'vue-router'
+import HomeView from "@/views/HomeView.vue";
+import AdminView from "@/views/AdminView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -6,12 +8,12 @@ const router = createRouter({
         {
             path: '/',
             name: 'home',
-            component: import('../views/HomeView.vue')
+            component: HomeView
         },
         {
             path: '/admin',
             name: 'admin',
-            component: import('../views/AdminView.vue')
+            component: AdminView
         }
     ]
 })
